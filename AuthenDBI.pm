d1d3cc88196a84cfc99c471a495a03cfe4402c2b
Edmund Mergl <E.Mergl@bawue.de>
1997-05-16 05:19:44 +0000

    Import of MERGL/ApacheDBI-0.4 from CPAN.
    gitpan-cpan-distribution: ApacheDBI
    gitpan-cpan-version:      0.4
    gitpan-cpan-path:         MERGL/ApacheDBI-0.4.tar.gz
    gitpan-cpan-author:       MERGL
    gitpan-cpan-maturity:     released

---
+++ AuthenDBI.pm
@@ -0,0 +1,168 @@
+package Apache::AuthenDBI;
+
+use Apache ();
+use Apache::Constants qw(OK AUTH_REQUIRED DECLINED SERVER_ERROR);
+use DBI ();
+
+use strict;
+
+#$Id: AuthenDBI.pm,v 1.4 1997/05/16 05:10:20 mergl Exp $
+
+require_version DBI 0.81;
+
+my $VERSION = '0.3';
+
+my $DEBUG = $ENV{APACHE_AUTHENDBI_DEBUG} || 0;
+
+
+my %Config = (
+    'AuthDBIDB'            => '',
+    'AuthDBIUser'          => '',
+    'AuthDBIAuth'          => '',
+    'AuthDBIDriver'        => '',
+    'AuthDBINameField'     => '',
+    'AuthDBIPasswordField' => '',
+    'AuthDBIUserTable'     => '',
+);
+
+sub handler {
+
+    my($r) = @_;
+
+    print STDERR "\nApache::AuthenDBI::handler\n" if $DEBUG;
+
+    return OK unless $r->is_initial_req; #only the first internal request
+
+    # here the dialog pops up and asks you for userid and password
+    my($res, $passwd_sent) = $r->get_basic_auth_pw;
+    print STDERR "get_basic_auth = $res, password sent = $passwd_sent\n" if $DEBUG;
+    return $res if $res; # e.g. HTTP_UNAUTHORIZED
+
+    my($key, $val);
+    my $attr = { };
+    while(($key, $val) = each %Config) {
+	$val = $r->dir_config($key) || $val;
+	$key =~ s/^AuthDBI//;
+	$attr->{$key} = $val;
+        printf STDERR "Config{ %-15s } = %s\n", $key, $val if $DEBUG;
+    }
+
+    my $dbh;
+    unless ($dbh = DBI->connect($attr->{DB}, $attr->{User}, $attr->{Auth}, $attr->{Driver})) {
+	$r->log_reason("db connect error with $attr->{DB}", $r->uri);
+	$r->note_basic_auth_failure;
+	return SERVER_ERROR;
+    }
+
+    my $user_sent = $dbh->quote($r->connection->user);
+    print STDERR "user sent = $user_sent, password sent = $passwd_sent \n" if $DEBUG;
+
+    my $statement = "SELECT $attr->{PasswordField} from $attr->{UserTable} WHERE $attr->{NameField}=$user_sent";
+    print STDERR "statement = $statement\n" if $DEBUG;
+
+    my $sth;
+    unless ($sth = $dbh->prepare($statement)) {
+	$r->log_reason("can not prepare statement: $DBI::errstr", $r->uri);
+	$r->note_basic_auth_failure;
+	return SERVER_ERROR;
+    }
+
+    my $rc;
+    unless ($rc = $sth->execute) {
+	$r->log_reason("can not execute statement: $DBI::errstr", $r->uri);
+	$r->note_basic_auth_failure;
+	return SERVER_ERROR;
+    }
+
+    my @row = $sth->fetchrow;
+    print STDERR "row = @row \n" if $DEBUG;
+
+    $sth->finish;
+
+    my $passwd;
+    unless ($passwd = $row[0]) {
+	$r->log_reason("User $user_sent not found", $r->uri);
+	$r->note_basic_auth_failure;
+	return AUTH_REQUIRED;
+    }
+
+    unless (crypt($passwd_sent, $passwd) eq $passwd) {
+	$r->log_reason("user $user_sent: password mismatch", $r->uri);
+	$r->note_basic_auth_failure;
+	return AUTH_REQUIRED;
+    }
+
+    $dbh->disconnect;
+
+    return OK;
+}
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Apache::AuthenDBI - Authentication via Perl's DBI
+
+=head1 SYNOPSIS
+
+ # Configuration in httpd.conf or srm.conf
+
+ PerlModule Apache::AuthenDBI
+
+ # Authentication in .htaccess
+
+ AuthName DBI
+ AuthType Basic
+
+ #authenticate via DBI
+ PerlAuthenHandler Apache::AuthenDBI
+
+ PerlSetVar AuthDBIDB     dbname
+ PerlSetVar AuthDBIUser   username
+ PerlSetVar AuthDBIAuth   auth
+ PerlSetVar AuthDBIDriver driver
+ #DBI->connect(DB, User, Auth, Driver)
+
+ PerlSetVar AuthDBIUserTable table
+ PerlSetVar AuthDBINameField user
+ PerlSetVar AuthDBIPasswordField password
+ #SELECT PasswordField from UserTable WHERE NameField='$user_sent'
+
+ <Limit GET POST>
+ require valid-user
+ </Limit>
+
+The AuthType is limited to Basic. The require directive is limited to 'valid-user' 
+and 'user xxx' (no group support). 
+
+=head1 DESCRIPTION
+
+This module allows the Apache server to authenticate against a database. 
+It should be used together with Apache::DBI.pm to gain the benefit of a
+persistent database connection. Remember that the authentification accesses 
+the database once for every request !  Make sure, that in httpd.conf or 
+srm.conf the module Apache::DBI comes first:
+
+ PerlModule Apache::DBI
+ PerlModule Apache::AuthenDBI
+
+The authentification module makes use of persistent connections only 
+if the appropriate module Apache::DBI is already loaded ! 
+
+
+The database access uses Perl's DBI. For supported DBI drivers see: 
+
+ http://www.hermetica.com/technologia/DBI/
+
+=head1 SEE ALSO
+
+Apache(3), DBI(3)
+
+=head1 AUTHORS
+
+ mod_perl by Doug MacEachern <dougm@osf.org>
+ DBI by Tim Bunce <Tim.Bunce@ig.co.uk>
+ Apache::AuthenDBI by Edmund Mergl <E.Mergl@bawue.de>
--- DBI.pm
+++ DBI.pm
@@ -1,123 +1,33 @@
 package Apache::DBI;
 
 use Apache ();
-use Apache::Constants qw(OK AUTH_REQUIRED DECLINED SERVER_ERROR);
 use DBI ();
 use strict;
 
-my $VERSION = '0.2';
+#$Id: DBI.pm,v 1.4 1997/05/11 20:25:40 mergl Exp $
 
-my $DEBUG = 0;
+require_version DBI 0.81;
 
-my (%Connected);
+my $VERSION = '0.3';
+
+my $DEBUG = $ENV{APACHE_DBI_DEBUG} || 0;
+
+my %Connected;
 
 sub connect {
 
-    my($self, @args) = @_;
+    my $class = shift;
+    unshift @_, $class if ref $class;
+    my $drh = shift;
+    my @args= @_;
+
     my $idx = join (":", (@args));
-    my $buf = '';
+    return $Connected{$idx} if $Connected{$idx} and $Connected{$idx}->ping;
 
-    return $Connected{$idx} if $Connected{$idx};
     print STDERR "Pid = $$, Apache::DBI::connect to '$idx'\n" if $DEBUG;
-    $Connected{$idx} = DBI->connect(@args);
-
-}
-
-
-sub disconnect {
-
-    my($dbh) = @_;
-    my($key, $value);
-
-    while (($key,$value) = each %Connected) {
-	last if $value == $dbh;
-    }
-
-    if ($value == $dbh) {
-        print STDERR "Pid = $$, Apache::DBI::disconnect of '$key'\n" if $DEBUG;
-        $value->disconnect;
-    } else {
-        print STDERR "Pid = $$, Apache::DBI::disconnect: couldn't find database handle !\n";
-    }
-}
-
-
-my %Config = (
-    'AuthDBIUser'          => '',
-    'AuthDBIAuth'          => '',
-    'AuthDBIDB'            => '',
-    'AuthDBIDriver'        => '',
-    'AuthDBINameField'     => '',
-    'AuthDBIPasswordField' => '',
-    'AuthDBIUserTable'     => '',
-);
-
-sub handler {
-
-    my($r) = @_;
-
-    print STDERR "\nApache::DBI::handler\n" if $DEBUG;
-
-    # here the dialog pops up and asks you for userid and password
-    my($res, $passwd_sent) = $r->get_basic_auth_pw;
-    print STDERR "get_basic_auth = $res, password sent = $passwd_sent\n" if $DEBUG;
-    return $res if $res; # return on first internal request (res = 401)
-
-    my($key, $val);
-    my $attr = { };
-    while(($key, $val) = each %Config) {
-	$val = $r->dir_config($key) || $val;
-	$key =~ s/^AuthDBI//;
-	$attr->{$key} = $val;
-        printf STDERR "Config{ %-15s } = %s\n", $key, $val if $DEBUG;
-    }
-
-    my $dbh;
-    unless ($dbh = Apache::DBI->connect($attr->{DB}, $attr->{User}, $attr->{Auth}, $attr->{Driver})) {
-	$r->log_reason("db connect error with $attr->{DB}", $r->uri);
-	$r->note_basic_auth_failure;
-	return SERVER_ERROR;
-    }
-
-    my $user_sent = $r->connection->user;
-    print STDERR "user sent = $user_sent \n" if $DEBUG;
-
-    my $statement = "SELECT $attr->{PasswordField} from $attr->{UserTable} WHERE $attr->{NameField}='$user_sent'";
-    print STDERR "statement = $statement" if $DEBUG;
-
-    my $sth;
-    unless ($sth = $dbh->prepare($statement)) {
-	$r->log_reason("can not prepare statement: $DBI::errstr", $r->uri);
-	$r->note_basic_auth_failure;
-	return SERVER_ERROR;
-    }
-
-    my $rc;
-    unless ($rc = $sth->execute) {
-	$r->log_reason("can not execute statement: $DBI::errstr", $r->uri);
-	$r->note_basic_auth_failure;
-	return SERVER_ERROR;
-    }
-
-    my @row = $sth->fetchrow;
-    print STDERR "row = @row \n" if $DEBUG;
-
-    $sth->finish;
-
-    my $passwd;
-    unless ($passwd = $row[0]) {
-	$r->log_reason("User '$user_sent' not found", $r->uri);
-	$r->note_basic_auth_failure;
-	return AUTH_REQUIRED;
-    }
-
-    unless (crypt($passwd_sent, $passwd) eq $passwd) {
-	$r->log_reason("user '$user_sent': password mismatch", $r->uri);
-	$r->note_basic_auth_failure;
-	return AUTH_REQUIRED;
-    }
-
-    return OK;
+    $Connected{$idx} = $drh->connect(@args);
+    $Connected{$idx}->{InactiveDestroy};
+    return $Connected{$idx};
 }
 
 
@@ -126,9 +36,9 @@ Apache::Status->menu_item(
     'DBI' => 'DBI connections',
     sub {
         my($r, $q) = @_;
-        my(@s) = qw(<TABLE><TR><TD>Database</TD><TD>Username</TD><TD>Driver</TD></TR>);
+        my(@s) = qw(<TABLE><TR><TD>Database</TD><TD>Username</TD></TR>);
         for (keys %Connected) {
-            push @s, '<TR><TD>', join('</TD><TD>', (split(':', $_))[0,1,3]), "</TD></TR>\n";
+            push @s, '<TR><TD>', join('</TD><TD>', (split(':', $_))[0,1]), "</TD></TR>\n";
         }
         push @s, '</TABLE>';
         return \@s;
@@ -143,7 +53,7 @@ __END__
 
 =head1 NAME
 
-Apache::DBI - Authenticate via Perl's DBI using a persistent database connection
+Apache::DBI - Initiate a persistent database connection
 
 =head1 SYNOPSIS
 
@@ -151,75 +61,49 @@ Apache::DBI - Authenticate via Perl's DBI using a persistent database connection
 
  PerlModule Apache::DBI
 
- # Authentication in .htaccess
-
- AuthName DBI
- AuthType Basic
-
- #authenticate via DBI
- PerlAuthenHandler Apache::DBI
-
- PerlSetVar AuthDBIDB     dbname
- PerlSetVar AuthDBIUser   username
- PerlSetVar AuthDBIAuth   auth
- PerlSetVar AuthDBIDriver driver
- #DBI->connect(qw(AuthDBIDB AuthDBIUser AuthDBIAuth AuthDBIDriver))
-
- PerlSetVar AuthDBIUserTable table
- PerlSetVar AuthDBINameField user
- PerlSetVar AuthDBIPasswordField password
-
- <Limit GET POST>
- require valid-user
- </Limit>
-
-The require directive is limited to 'valid-user' and 'user xxx' (no group 
-support). 
-
- # Persistent database connection in CGI script
-
- use Apache::DBI;
-
- $dbh = Apache::DBI->connect(...);
-
 =head1 DESCRIPTION
 
-This module consists out of two parts which can be used 
-independently:
-
-it allows the apache server to authenticate against a database 
-and it provides a persistent database connection. 
-
-The authentication initiates a persistent database conection, 
-but the persistent database connection can also be used without 
-authentication. 
+This module initiates a persistent database connection. 
 
 The database access uses Perl's DBI. For supported DBI drivers see: 
 
  http://www.hermetica.com/technologia/DBI/
 
-For using the persistent database connection all you really need is 
-to replace DBI with Apache::DBI. When connecting to a database the 
-module looks if a database handle from a previous connect request is 
-already stored. If not, a new connection is established and the handle 
-is stored for later re-use. There is no need to delete the disconnect
-statements from your code. They won't do anything. If you want to do an 
-explicit disconnect you need to call: Apache::DBI::disconnect($dbh).
+When connecting to a database the module looks if a database handle 
+from a previous connect request is already stored. If not, a new 
+connection is established and the handle is stored for later re-use. 
+There is no need to delete the disconnect statements from your code. 
+They won't do anything because this module hides the database handle. 
 
 The Apache::DBI module still has a limitation: it keeps database 
 connections persistent on a per process basis. The problem is, if 
-a user accesses several times a database during one session, the http 
-requests will be handled very likely by different httpd children. Every 
-child process needs to do its own connect. It would be nice, if all httpd 
-children could share the database handles. I still don't know how to solve 
-this problem. So if anyone knows how to do this, please let me know. 
+a user accesses several times a database, the http requests will be 
+handled very likely by different httpd children. Every child process 
+needs to do its own connect. It would be nice, if all httpd children 
+could share the database handles. One possible solution might be a 
+threaded Apache version. 
+
+With this limitation in mind, there are scenarios, where the usage of 
+Apache::DBI.pm is depreciated. Think about a heavy loaded Web-site where 
+every user connects to the database with a unique userid. Every httpd 
+child would create many database handles each of which spawning a new 
+backend process. In a short time this would kill the web server. 
+
+Another problem are timeouts: some databases disconnect the client 
+after a certain time of inactivity. The module tries to validate the 
+database handle using the new ping-method of the DBI-module. This 
+method returns true as default. If the database handle is not valid 
+and the driver module has no implementation for the ping method, you 
+will get an error when accessing the database. As a work-around you can 
+try to replace the ping method by any database command, which is cheap 
+and safe. 
 
 This module plugs in a menu item for Apache::Status. The menu lists 
 the current database connections. It should be considered incomplete 
-because of the limitations explained above. It shows the current database 
-connections for one specific httpd process, the one which happens to 
-serve the current request. Other httpd children might have other 
-database connections. 
+because of the limitations explained above. It shows the current 
+database connections for one specific httpd process, the one which 
+happens to serve the current request. Other httpd children might have 
+other database connections. 
 
 
 =head1 SEE ALSO
@@ -227,9 +111,6 @@ database connections.
 Apache(3), DBI(3)
 
 =head1 AUTHORS
-
-The authentication part is taken from Apache::AuthenDBI and rewritten, 
-so that it does not depend anymore on HTTPD.
 
  mod_perl by Doug MacEachern <dougm@osf.org>
  DBI by Tim Bunce <Tim.Bunce@ig.co.uk>
